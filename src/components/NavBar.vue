ffcadf741dd0a31e6b48aa5cda6547546753453a
Rafael Takano <rafael-takano@hotmail.com>
2022-07-12 02:49:34 -0300

    feat: created navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav id="nav-bar">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+}
+
+nav a.router-link-exact-active {
+  color: var(--primary-color);
+}
+</style>
